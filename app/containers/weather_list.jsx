470cdb0b73eb10a2db17692957606f3c84bde332
Phanor Coll <phanorcoll@gmail.com>
2017-11-04 15:46:12 -0400

    Weather list container
    created the container and added the initial design.. TODO: Fix the error with sparklines chart

---
+++ app/containers/weather_list.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+class WeatherList extends Component {
+
+    renderWeather = (cityData) => {
+        let name = cityData.city.name;
+        let temps = cityData.list.map(weather => weather.main.temp);
+
+        return (
+            <tr key={name}>
+                <td>
+                    {name}
+                </td>
+                <td>
+                    <Sparkline height={120} width={180} data={temps}>
+                        <SparklineLine color="red"/>
+                    </Sparkline>
+                </td>
+            </tr>
+        );
+    }
+
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+function mapStateToProps({ weather }) {
+    return { weather }
+}
+
+export default connect(mapStateToProps)(WeatherList)
